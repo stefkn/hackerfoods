a4b1bb4a36621cb8701cc465a424ab0b837c213a
stefkn <stefan.nowak@tails.com>
2024-06-11 01:11:31 +0100

    create and add initial models and migrations for Recipe and Ingredient

--- hackerfoods_app/models.py
+++ hackerfoods_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images/", blank=True)
+    ingredients = models.ManyToManyField("Ingredient")
+    instructions = models.TextField()
+    cooking_time = models.IntegerField()
+    difficulty = models.CharField(max_length=20)
+    servings = models.IntegerField()
+    is_vegetarian = models.BooleanField(default=False)
+    is_vegan = models.BooleanField(default=False)
+    is_gluten_free = models.BooleanField(default=False)
+    is_dairy_free = models.BooleanField(default=False)
+    is_nut_free = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images/", blank=True)
+
+    def __str__(self):
+        return self.name
