7170e53f8b6546db5961206f12810d961a1a912d
stefkn <stefan.nowak@tails.com>
2024-06-11 12:25:37 +0100

    set up a basic recipe view

--- hackerfoods_app/urls.py
+++ hackerfoods_app/urls.py
@@ -4,4 +4,5 @@ from hackerfoods_app import views
 urlpatterns = [
     path("", views.home, name='home'),
     path("about", views.about, name='about'),
+    path("<int:recipe_id>/", views.detail, name="detail"),
 ]
--- hackerfoods_app/views.py
+++ hackerfoods_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Recipe
 
 def home(request):
-    return render(request, "home.html", {})
+    recipes = Recipe.objects.all()
+    context = {"recipes": recipes}
+    return render(request, "home.html", context)
 
 def about(request):
     return render(request, "about.html", {})
 
+def detail(request, recipe_id):
+    recipe = Recipe.objects.get(pk=recipe_id)
+    context = {"recipe": recipe}
+    return render(request, "recipe.html", context)
