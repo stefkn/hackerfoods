b69c34abc80fcdc1fd1b1568a5fa33792bd21894
stefkn <stefan.nowak@tails.com>
2024-06-14 00:59:59 +0100

    refactor and update app view functions

--- hackerfoods_app/views.py
+++ hackerfoods_app/views.py
@@ -1,32 +1,42 @@
-from django.shortcuts import render
-from .models import Recipe
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Count, Sum
+from .models import Recipe, Vote
 
-def home(request):
-    recipes = Recipe.objects.all()
+
+def return_partial_or_full(request, partial_template, full_template):
+    if request.htmx:
+        template_name = partial_template
+    else:
+        template_name = full_template
+
+    return template_name
+
+
+def recipes(request):
+    recipes = Recipe.objects.all().annotate(
+        vote_sum=Sum("vote__vote_value")
+    )
     context = {"recipes": recipes}
 
-    if request.htmx:
-        template_name = "home.html"
-    else: 
-        template_name = "full/home_full.html"
-    
+    template_name = return_partial_or_full(
+        request, "recipes.html", "full/recipes_full.html"
+    )
+
     return render(request, template_name, context)
 
+
 def about(request):
-    if request.htmx:
-        template_name = "about.html"
-    else: 
-        template_name = "full/about_full.html"
+    template_name = return_partial_or_full(
+        request, "about.html", "full/about_full.html"
+    )
 
     return render(request, template_name, {})
 
-def detail(request, recipe_id):
-    recipe = Recipe.objects.get(pk=recipe_id)
-    context = {"recipe": recipe}
 
-    if request.htmx:
-        template_name = "recipe_detail.html"
-    else: 
-        template_name = "full/recipe_detail_full.html"
+def detail(request, recipe_id):
+    context = {"recipe": get_object_or_404(Recipe, pk=recipe_id)}
+    template_name = return_partial_or_full(
+        request, "recipe_detail.html", "full/recipe_detail_full.html"
+    )
 
     return render(request, template_name, context)
