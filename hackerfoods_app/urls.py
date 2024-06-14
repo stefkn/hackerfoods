961712b7fe0418feefdb6cb9b805c8357397dc8c
stefkn <stefan.nowak@tails.com>
2024-06-14 01:00:12 +0100

    rename index to recipes

--- hackerfoods_app/urls.py
+++ hackerfoods_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from hackerfoods_app import views
 
+app_name = "hackerfoods_app"
+
 urlpatterns = [
-    path("", views.home, name='home'),
+    path("", views.recipes, name='recipes'),
     path("about", views.about, name='about'),
     path("<int:recipe_id>/", views.detail, name="detail"),
 ]
