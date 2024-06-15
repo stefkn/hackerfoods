b22a58b78b1aeb3b36cccf5485ed95dabf0da67f
stefkn <stefan.nowak@tails.com>
2024-06-15 01:02:05 +0100

    refactor SignUpView to use partial or full template based on request

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.shortcuts import render
-
+from django.contrib import auth
 from .forms import CustomUserCreationForm
-
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "signup.html"
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def return_partial_or_full(request, partial_template, full_template):
@@ -17,6 +13,16 @@ def return_partial_or_full(request, partial_template, full_template):
         template_name = full_template
 
     return template_name
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+
+    def get(self, request, *args, **kwargs):
+        template_name = return_partial_or_full(
+            request, "signup.html", "full/signup_full.html"
+        )
+        return render(request, template_name, {"form": self.form_class})
 
 def home(request):
     template_name = return_partial_or_full(
