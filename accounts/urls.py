6245779422bbce436c72e50d26f5312df0866b46
stefkn <stefan.nowak@tails.com>
2024-06-15 01:01:44 +0100

    feat: Add login and logout functionality to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,8 @@ app_name = "accounts"
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("home", views.home, name='home'),
     path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
